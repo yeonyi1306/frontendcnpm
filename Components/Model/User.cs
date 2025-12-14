6397ce48a0228e67372a7cb264c617515ef4b743
Yeonyi <parksumin206@gmail.com>
2025-12-14 19:17:08 +0700

    Initial commit of FrontEnd 8.0

---
+++ Components/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBlazorApp.Components.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = "";
+        public string FullName { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string Role { get; set; } = "";
+    }
+
+}
